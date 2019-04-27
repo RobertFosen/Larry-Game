f5c0b5a87fcb2e7f47969e258bc259ebfb965fe8
unknown <robertcfosen@lewisu.edu>
2019-04-27 17:23:56 -0500

    first push to github

---
+++ Assets/ArmScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmScript : MonoBehaviour
+{
+
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+
+        if (vertical > 0)
+        {
+            anim.SetInteger("direction", 1);
+        }
+        else if (vertical == 0)
+        {
+            anim.SetInteger("direction", 0);
+        }
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float moveSpeed = 100f;
+    public float turnSpeed = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            if (!(Input.GetKey("w") || Input.GetKey("s")))
+            {
+                transform.Rotate(Vector3.up, -(turnSpeed * 2) * Time.deltaTime);
+            }
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            if (!(Input.GetKey("w") || Input.GetKey("s"))) {
+                transform.Rotate(Vector3.up, (turnSpeed * 2) * Time.deltaTime);
+            }
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        }
+    }
+}
